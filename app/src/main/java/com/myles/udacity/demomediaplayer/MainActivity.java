9d7d1ba54972ceea388cd7f424f20ed97ec6a2b2
Myles Ieong <Myles Ieong>
2016-11-15 00:23:35 +0800

    Add demo code

--- app/src/main/java/com/myles/udacity/demomediaplayer/MainActivity.java
+++ app/src/main/java/com/myles/udacity/demomediaplayer/MainActivity.java
@@ -1,30 +1,38 @@
 package com.myles.udacity.demomediaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playButton = (Button)this.findViewById(R.id.play_button);
+        this.player = MediaPlayer.create(this, R.raw.song);
+
+        final Button playButton = (Button)this.findViewById(R.id.play_button);
         playButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Play Pressed", Toast.LENGTH_SHORT).show();
+                player.start();
+                //Toast.makeText(MainActivity.this, "Play Pressed", Toast.LENGTH_SHORT).show();
             }
         });
         Button pauseButton = (Button)this.findViewById(R.id.pause_button);
         pauseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Pause Pressed", Toast.LENGTH_SHORT).show();
+                player.pause();
+                //Toast.makeText(MainActivity.this, "Pause Pressed", Toast.LENGTH_SHORT).show();
             }
         });
 
